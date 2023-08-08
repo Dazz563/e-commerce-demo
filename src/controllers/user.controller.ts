395ee64acfc459ab8324fdce3b3b6fddf9248414
Darren <staysignal@outlook.com>
2023-08-08 14:16:43 +0200

    updates to user controller file

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,6 @@
 import {NextFunction, Request, Response} from 'express';
+import * as csvParser from 'csv-parser';
+import * as fs from 'fs';
 
 export const getMeHandler = async (req: Request, res: Response, next: NextFunction) => {
 	try {
@@ -14,3 +16,35 @@ export const getMeHandler = async (req: Request, res: Response, next: NextFuncti
 		next(err);
 	}
 };
+
+// export const getUserWithPostsToCsvHandler = async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//         const user = res.locals.user;
+
+//         const posts = await user.getPosts();
+
+//         const csvData = posts.map((post) => {
+//             return {
+//                 id: post.id,
+//                 title: post.title,
+//                 content: post.content,
+//                 createdAt: post.createdAt,
+//                 updatedAt: post.updatedAt,
+//             };
+//         });
+
+//         res.setHeader('Content-Type', 'text/csv');
+//         res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'user.csv\"');
+//         res.setHeader('Cache-Control', 'no-cache');
+//         res.setHeader('Pragma', 'no-cache');
+
+//         res.status(200).status(200).json({
+//             status: 'success',
+//             data: {
+//                 user,
+//             },
+//         });
+//     } catch (err: any) {
+//         next(err);
+//     }
+// };
